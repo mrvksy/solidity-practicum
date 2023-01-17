f44ac43d4f3cc18c100e301e6a3a211c462a0b9b
Merve Aksoy <mrvksy@outlook.com>
2023-01-17 21:38:46 +0300

    update counter.sol

--- Counter.sol
+++ Counter.sol
@@ -6,10 +6,11 @@ pragma solidity ^0.8.7;
 contract Counter {
     uint public count;
 
+    // Function to increment count by 1
     function inc() public {
         count += 1;
     }
-    
+    // Function to decrement count by 1
     function dec() public {
         count -= 1;
         }
