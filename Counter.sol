7d9e41ad4dd3eea0538154e86405f62511e60494
Merve Aksoy <mrvksy@outlook.com>
2023-01-17 21:31:59 +0300

    added count

--- Counter.sol
+++ Counter.sol
@@ -1,1 +1,16 @@
 
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+
+contract Counter {
+    uint public count;
+
+    function inc() public {
+        count += 1;
+    }
+    
+    function dec() public {
+        count -= 1;
+        }
+}
